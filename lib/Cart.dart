09ab995fabe85e64de3f966e0f41f98d3cd5b187
ryanvarghese <ryanvarghese12@student.sfit.ac.in>
2021-02-26 21:14:05 +0530

    second commit

---
+++ lib/Cart.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:sfit_canteen_app_new/menu.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Cart extends StatefulWidget {
+  @override
+  CartState createState() => CartState();
+}
+
+class CartState extends State<Cart> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: AppBar(
+          leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: (){
+              Navigator.pop(context);
+          }, ),
+        backgroundColor: Colors.orange,
+        ),
+        body: Container(
+          child: Center(
+        child: Text('Cart'),
+        ),
+        ),
+      )
+    );
+  }
+}
